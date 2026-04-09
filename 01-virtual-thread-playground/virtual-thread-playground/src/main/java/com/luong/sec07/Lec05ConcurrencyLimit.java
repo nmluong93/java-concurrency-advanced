2966e387cff2152e3a9aebda0062259949f86c63
luongnguyen <luongnm93@live.com>
2026-04-09 21:24:18 +0700

    Don't pool the Virtual Thread (e.g using Virtual Thread factory in newFixedThreadPool) based on Oracle Team warning

---
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/luong/sec07/Lec05ConcurrencyLimit.java
@@ -0,0 +1,36 @@
+package com.luong.sec07;
+
+import com.luong.sec07.externalservice.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Lec05ConcurrencyLimit {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec05ConcurrencyLimit.class);
+
+    public static void main(String[] args) {
+        // It is NOT supposed to be used this way as it pools Virtual Threads
+        var factory = Thread.ofVirtual().name("luong", 1).factory();
+//        execute(Executors.newFixedThreadPool(3, factory), 20);
+        execute(Executors.newFixedThreadPool(3), 20);
+    }
+
+    private static void execute(ExecutorService executorService, int taskCount){
+        try(executorService){
+            for (int i = 1; i <= taskCount; i++) {
+                int j = i;
+                executorService.submit(() -> printProductInfo(j));
+            }
+            log.info("submitted");
+        }
+    }
+
+    // 3rd party service
+    // contract: 3 concurrent calls are allowed
+    private static void printProductInfo(int id){
+        log.info("{} => {}", id, Client.getProduct(id));
+    }
+}
