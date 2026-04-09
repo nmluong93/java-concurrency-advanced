06e9c2c620436cf5273a18fada78aaab2a35244e
luongnguyen <luongnm93@live.com>
2026-04-09 20:49:02 +0700

    Testing REST API with Executors.newVirtualThreadPerTaskExecutor for Non-Blocking IO benefit with help of Virtual Thread

---
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/luong/sec07/Lec03AccessResponseUsingFuture.java
@@ -0,0 +1,34 @@
+package com.luong.sec07;
+
+import com.luong.sec07.externalservice.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+/*
+    To get multiple products information in parallel
+ */
+public class Lec03AccessResponseUsingFuture {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec03AccessResponseUsingFuture.class);
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        try(var executor = Executors.newVirtualThreadPerTaskExecutor()){
+
+            var product1 = executor.submit(() -> Client.getProduct(1));
+            var product2 = executor.submit(() -> Client.getProduct(2));
+            var product3 = executor.submit(() -> Client.getProduct(3));
+
+            log.info("product-1: {}", product1.get());
+            log.info("product-2: {}", product2.get());
+            log.info("product-3: {}", product3.get());
+
+        }
+        log.info("finish");
+    }
+
+
+}
