644a40c7bf529033280a7606d68a35add9d2464b
luongnguyen <luongnm93@live.com>
2026-04-10 09:24:30 +0700

    Handle timeout exception in CompletableFuture

--- 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/luong/sec08/aggregator/AggregatorService.java
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/luong/sec08/aggregator/AggregatorService.java
@@ -1,6 +1,8 @@
 package com.luong.sec08.aggregator;
 
 import com.luong.sec08.externalservice.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -8,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class AggregatorService {
 
+    private static final Logger log = LoggerFactory.getLogger(AggregatorService.class);
     private final ExecutorService executorService;
 
     public AggregatorService(ExecutorService executorService) {
@@ -16,12 +19,23 @@ public class AggregatorService {
 
     public ProductDto getProductDto(int id) {
         var product = CompletableFuture.supplyAsync(() -> Client.getProduct(id), executorService)
+                                        // handle timeout
                                        .orTimeout(1250, TimeUnit.MILLISECONDS)
-                                       .exceptionally(ex -> null);
+                                       .exceptionally(ex -> {
+                                           log.error("get product error", ex);
+                                           return null;
+                                       });
         var rating = CompletableFuture.supplyAsync(() -> Client.getRating(id), executorService)
-                                      .exceptionally(ex -> -1)
+                                      .exceptionally(ex -> {
+                                          log.warn("get rating failed {}", ex.getMessage());
+                                          return -1;
+                                      })
+                                      // handle timeout
                                       .orTimeout(1250, TimeUnit.MILLISECONDS)
-                                      .exceptionally(ex -> -2);
+                                      .exceptionally(ex -> {
+                                          log.warn("get rating timeout {}", ex.getMessage());
+                                          return -2;
+                                      });
         return new ProductDto(id, product.join(), rating.join());
     }
 
